b39c6a4b93c65277ec08ccfdc265a1b94f5d3927
Henry Gardiner <henrygdnr@gmail.com>
2025-05-30 23:12:07 -0400

    Added officer data.
    No implementation done yet

--- src/Pages/Officers.jsx
+++ src/Pages/Officers.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 
 function Officers() {
+  const president = { name: "Michelle Chung", role: "President", sanrio: "Pochacco", photo: "" };
+  const members = [
+    { name: "Darren Zheng", role: "Vice President", sanrio: "Tuxedo Sam", photo: "" },
+    { name: "Brennen Le", role: "Fundraising Chair", sanrio: "Hangyodon", photo: "" },
+    { name: "Megan Liu", role: "Treasurer", sanrio: "Keroppi", photo: "" },
+    { name: "Betty Au", role: "Secretary", sanrio: "Hanamaruobake", photo: "" },
+    { name: "Ijeoma Nwaka", role: "Graphic Designer", sanrio: "Cinnamoroll", photo: "" },
+    { name: "Kristi Sunwar", role: "Graphic Designer", sanrio: "Unknown", photo: "" },
+    { name: "Rhea Patel", role: "PR Chair", sanrio: "Gudetama", photo: "" },
+    { name: "Davy Ling", role: "Culture Chair", sanrio: "Pekkle", photo: "" },
+    { name: "Rowena Liu", role: "Historian", sanrio: "Hello Kitty", photo: "" },
+    { name: "Alvin Zhou", role: "Videographer", sanrio: "Unknown", photo: "" },
+    { name: "Ruka Fakoya", role: "Advisor", sanrio: "Pompompurin", photo: "" }
+  ];
+
+
   return (
     <div>Officers</div>
   )
